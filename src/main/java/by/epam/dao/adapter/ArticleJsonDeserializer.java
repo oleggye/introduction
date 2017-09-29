970c2bac9760680a0e02bf12bc46f35d9233789c
Oleg Maksimovich <Oleg_Maksimovich@epam.com>
2017-09-29 18:51:01 +0300

    fix json deserializer

--- src/main/java/by/epam/dao/adapter/ArticleJsonDeserializer.java
+++ src/main/java/by/epam/dao/adapter/ArticleJsonDeserializer.java
@@ -12,6 +12,7 @@ import java.io.IOException;
 public class ArticleJsonDeserializer extends JsonDeserializer<Article> {
 
     private static final String DEFAULT_AUTHOR_NAME = "Unknown";
+    private static final String DEFAULT_SAFETY_VALUE = "";
 
     private static final String ARTICLE_NODE_TAG = "article";
     private static final String TITLE_NODE_TAG = "title";
@@ -23,13 +24,21 @@ public class ArticleJsonDeserializer extends JsonDeserializer<Article> {
         JsonNode node = jsonParser.getCodec().readTree(jsonParser);
         JsonNode articleNode = node.get(ARTICLE_NODE_TAG);
 
-        String title = articleNode.get(TITLE_NODE_TAG).asText();
-        String authorName = articleNode.get(AUTHOR_NODE_TAG).asText();
-        String contents = articleNode.get(CONTENTS_NODE_TAG).asText();
+        String title = safetyGetElement(articleNode.get(TITLE_NODE_TAG));
+        String authorName = safetyGetElement(articleNode.get(AUTHOR_NODE_TAG));
+        String contents = safetyGetElement(articleNode.get(CONTENTS_NODE_TAG));
 
         if (authorName.isEmpty()) {
             authorName = DEFAULT_AUTHOR_NAME;
         }
         return new Article(title, new Author(authorName), contents);
+    }
+
+    private String safetyGetElement(JsonNode jsonNode) {
+        if (jsonNode == null) {
+            return DEFAULT_SAFETY_VALUE;
+        } else {
+            return jsonNode.asText();
+        }
     }
 }
